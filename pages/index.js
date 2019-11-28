f282ec5bd766cdc7671fba7cf51f206cb55b153b
Peter Ovchinnikov <peter@boarlabs.xyz>
2019-11-28 12:22:26 +0200

    Basic template

--- pages/index.js
+++ pages/index.js
@@ -1,87 +1,59 @@
+//import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react'
 import Head from 'next/head'
-import Nav from '../components/nav'
+import Nav from 'react-bootstrap/Nav'
+
+
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Navbar from 'react-bootstrap/Navbar'
 
 const Home = () => (
   <div>
     <Head>
-      <title>Home</title>
+      <title>Flowers way. Країна очима велосипедистів</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
-    <Nav />
+    <Container>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Flowers way</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
 
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+          <Nav
+            activeKey="/"
+            onSelect={selectedKey => alert(`selected ${selectedKey}`)}
+          >
+            <Nav.Item>
+              <Nav.Link href="#pathfinder">Pathfinder</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#blog">Blog</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#shop">Shop</Nav.Link>
+            </Nav.Item>
+          </Nav>
 
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
-      </div>
-    </div>
+        </Navbar.Collapse>
+      </Navbar>
+      <Row><Col><h1>Flowers way<br /> Країна очима велосипедистів</h1></Col></Row>
+      {/* Projects */}
+      <Row>
+        <Col>Our Projects</Col>
+      </Row>
+      <Row>
+        <Col>Pathfinder</Col>
+        <Col>Blog</Col>
+        <Col>Equipment</Col>
+      </Row>
+      <Row>
+        <Col>About Us</Col>
+      </Row>
+    </Container>
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
   </div>
 )
 
