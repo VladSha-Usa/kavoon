811dbacf8c88c11b383f2de96709a8e450802f93
Peter Ovchinnikov <peter@boarlabs.xyz>
2019-12-11 23:50:36 +0200

    Add analytics

--- pages/index.js
+++ pages/index.js
@@ -46,13 +46,29 @@ const Home = () => {
     preloadImage('img/viber.svg')
     preloadImage('img/instagram.svg')
     preloadImage('img/facebook.svg')
+
+    var firebaseConfig = {
+      apiKey: "AIzaSyC8mAIUcvd7bqw46bajWEbRrHcDTnd1qTw",
+      authDomain: "flowers-way.firebaseapp.com",
+      databaseURL: "https://flowers-way.firebaseio.com",
+      projectId: "flowers-way",
+      storageBucket: "flowers-way.appspot.com",
+      messagingSenderId: "305623494289",
+      appId: "1:305623494289:web:fa6954b42047c910079930",
+      measurementId: "G-PSBJYP7E5X"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig)
+    firebase.analytics()
+
   });
   return (<>
     <Head>
       <link href="https://fonts.googleapis.com/css?family=Archivo+Black&display=swap" rel="stylesheet" />
       <link href="https://fonts.googleapis.com/css?family=Montserrat:500,600,900&display=swap" rel="stylesheet" />
-      <script src="https://www.gstatic.com/firebasejs/7.5.2/firebase-analytics.js"></script>
       <script src="https://www.gstatic.com/firebasejs/7.5.2/firebase-app.js"></script>
+      <script src="https://www.gstatic.com/firebasejs/7.5.2/firebase-analytics.js"></script>
+
     </Head>
 
     <div id="home" className="map-image">
