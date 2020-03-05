1c2e9ded932af22ccf834f03bab902e6c5607e39
vlad <newshapovalov2002@gmail.com>
2020-03-05 20:07:24 +0200

    Remove .titleThirdClass

--- components/mainPage/title.js
+++ components/mainPage/title.js
@@ -3,16 +3,13 @@ const Title = ({ classPrefix }) => {
   const prefix = `${classPrefix ? classPrefix + '-' : ""}title`
   const titleFirstClass = `${prefix}-first`
   const titleSecondClass = `${prefix}-second`
-  const titleThirdClass = `${prefix}-third`
   const backgroundClass = `${prefix}-background`
   
   const mainFontSize = 128;
-  const thirdFontSize = 18;
   return (<>
     <div className={backgroundClass}>FLOWERS<br /> WAY</div>
     <div className={titleFirstClass}>FLOWERS</div>
     <div className={titleSecondClass}>WAY</div>
-    <div className={titleThirdClass}>Країна очима велосипедистів</div>
     <style jsx>{`
 
      .${titleFirstClass} {
@@ -29,19 +26,6 @@ const Title = ({ classPrefix }) => {
         line-height: 1.18;
         letter-spacing: 5.98px;
         color: #1831aa;
-      }
-      .${titleThirdClass} {
-        font-family: Montserrat;
-        font-size: ${thirdFontSize}px;
-        font-weight: 600;
-        letter-spacing: 0.84px;
-        color: var(--texticonscolor);
-
-        text-transform: uppercase;
-        max-width: 380px;
-        position: relative;
-        top: -158px;
-        left: 360px;
       }
       .${backgroundClass} {
         object-fit: contain;
@@ -69,12 +53,6 @@ const Title = ({ classPrefix }) => {
         .${titleSecondClass} {
           font-size: 104px;
         }
-        .${titleThirdClass} {
-          font-size: 15px;
-          letter-spacing: 0.54px;
-          top: -138px;
-          left: 310px;
-        } 
       }
 
       @media only screen and (max-width: 1023px) {
@@ -97,12 +75,6 @@ const Title = ({ classPrefix }) => {
           //text-align: center;
           font-size: 58px;
           line-height: 62px;
-        }
-        .${titleThirdClass} {
-          //text-align: center;
-          font-size: 16px;
-          top: 0px;
-          left: 0px;
         } 
       }
 
