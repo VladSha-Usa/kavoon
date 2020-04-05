8120a8123741d9f9077ff1473098753f85c094e3
Vlad Shapovalov <newshapovalov2002@gmail.com>
2020-04-05 13:10:22 +0300

    Add padding for price in order to reach PixPerf

--- components/shop/goodsList.js
+++ components/shop/goodsList.js
@@ -11,56 +11,56 @@ const GoodsList = () => {
           srcSet="img/img-shop-item/photo-1@2x.jpg 2x, img/img-shop-item/photo-1@3x.jpg 3x,"
           contentTitle={`Сумка-тубус на вилку\nі ще щось у 2му рядку`}
           capacity="10"
-          price="1300"
+          price="1 300"
         ></GoodListItem>
         <GoodListItem
           src="img/img-shop-item/photo-2.jpg"
           srcSet="img/img-shop-item/photo-2@2x.jpg 2x, img/img-shop-item/photo-2@3x.jpg 3x,"
           contentTitle={`Сумка-тубус\nна вилку.`}
           capacity="10"
-          price="1300"
+          price="1 300"
         ></GoodListItem>
         <GoodListItem
           src="img/img-shop-item/photo-3.jpg"
           srcSet="img/img-shop-item/photo-3@2x.jpg 2x, img/img-shop-item/photo-3@3x.jpg 3x,"
           contentTitle={`Сумка-тубус\nна вилку.`}
           capacity="10"
-          price="1300"
+          price="1 300"
         ></GoodListItem>
         <GoodListItem
           src="img/img-shop-item/photo-4.jpg"
           srcSet="img/img-shop-item/photo-4@2x.jpg 2x, img/img-shop-item/photo-4@3x.jpg 3x,"
           contentTitle={`Сумка-тубус\nна вилку.`}
           capacity="6"
-          price="1300"
+          price="1 300"
         ></GoodListItem>
         <GoodListItem
           src="img/img-shop-item/photo-5.jpg"
           srcSet="img/img-shop-item/photo-5@2x.jpg 2x, img/img-shop-item/photo-5@3x.jpg 3x,"
           contentTitle={`Сумка-бананка\nна кермо`}
           capacity="3"
-          price="1300"
+          price="1 300"
         ></GoodListItem>
         <GoodListItem
           src="img/img-shop-item/photo-6.jpg"
           srcSet="img/img-shop-item/photo-6@2x.jpg 2x, img/img-shop-item/photo-6@3x.jpg 3x,"
           contentTitle="Бананка"
           capacity="10"
-          price="1300"
+          price="1 300"
         ></GoodListItem>
         <GoodListItem
           src="img/img-shop-item/photo-7.jpg"
           srcSet="img/img-shop-item/photo-7@2x.jpg 2x, img/img-shop-item/photo-7@3x.jpg 3x,"
           contentTitle="Внутрірамна сумка"
           capacity="10"
-          price="1300"
+          price="1 300"
         ></GoodListItem>
         <GoodListItem
           src="img/img-shop-item/photo-8.jpg"
           srcSet="img/img-shop-item/photo-8@2x.jpg 2x, img/img-shop-item/photo-8@3x.jpg 3x,"
           contentTitle={`Сумка-тубуc\nна вилку`}
           capacity="10"
-          price="1300"
+          price="1 300"
         ></GoodListItem>
       </div>
       <style jsx>{`
