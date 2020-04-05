1e3992f63c73160755b8e3c87def9a60920ca3ce
Vlad Shapovalov <newshapovalov2002@gmail.com>
2020-04-05 12:37:37 +0300

    Add  \n(transfer) in order to reach PixPerf

--- components/shop/goodsList.js
+++ components/shop/goodsList.js
@@ -9,35 +9,35 @@ const GoodsList = () => {
         <GoodListItem
           src="img/img-shop-item/photo-1.jpg"
           srcSet="img/img-shop-item/photo-1@2x.jpg 2x, img/img-shop-item/photo-1@3x.jpg 3x,"
-          contentTitle="Сумка-тубус на вилку і ще щось у 2му рядку"
+          contentTitle={`Сумка-тубус на вилку\nі ще щось у 2му рядку`}
           capacity="10"
           price="1300"
         ></GoodListItem>
         <GoodListItem
           src="img/img-shop-item/photo-2.jpg"
           srcSet="img/img-shop-item/photo-2@2x.jpg 2x, img/img-shop-item/photo-2@3x.jpg 3x,"
-          contentTitle="Сумка-тубус на вилку."
+          contentTitle={`Сумка-тубус\nна вилку.`}
           capacity="10"
           price="1300"
         ></GoodListItem>
         <GoodListItem
           src="img/img-shop-item/photo-3.jpg"
           srcSet="img/img-shop-item/photo-3@2x.jpg 2x, img/img-shop-item/photo-3@3x.jpg 3x,"
-          contentTitle="Сумка-тубус"
+          contentTitle={`Сумка-тубус\nна вилку.`}
           capacity="10"
           price="1300"
         ></GoodListItem>
         <GoodListItem
           src="img/img-shop-item/photo-4.jpg"
           srcSet="img/img-shop-item/photo-4@2x.jpg 2x, img/img-shop-item/photo-4@3x.jpg 3x,"
-          contentTitle="Сумка-тубус на вилку"
+          contentTitle={`Сумка-тубус\nна вилку.`}
           capacity="6"
           price="1300"
         ></GoodListItem>
         <GoodListItem
           src="img/img-shop-item/photo-5.jpg"
           srcSet="img/img-shop-item/photo-5@2x.jpg 2x, img/img-shop-item/photo-5@3x.jpg 3x,"
-          contentTitle="Сумка-бананка на кермо"
+          contentTitle={`Сумка-бананка\nна кермо`}
           capacity="3"
           price="1300"
         ></GoodListItem>
@@ -58,7 +58,7 @@ const GoodsList = () => {
         <GoodListItem
           src="img/img-shop-item/photo-8.jpg"
           srcSet="img/img-shop-item/photo-8@2x.jpg 2x, img/img-shop-item/photo-8@3x.jpg 3x,"
-          contentTitle="Сумка-тубус на вилку"
+          contentTitle={`Сумка-тубуc\nна вилку`}
           capacity="10"
           price="1300"
         ></GoodListItem>
