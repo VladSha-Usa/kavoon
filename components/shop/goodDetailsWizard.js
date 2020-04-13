f6ae9c44f7255139fb45b16bf2d50e52b6e6ce55
Vlad Shapovalov <newshapovalov2002@gmail.com>
2020-04-13 17:55:44 +0300

    Add wizard wireframe

---
+++ components/shop/goodDetailsWizard.js
@@ -0,0 +1,10 @@
+const GoodDetailsWizard = () => {
+  const wizardWrap = "good-wizard-wrapper";
+  return (
+    <>
+      <div className={wizardWrap}></div>
+      <style jsx>{``}</style>
+    </>
+  );
+};
+export default GoodDetailsWizard;
