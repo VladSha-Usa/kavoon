10ce246a20c6361bbc55ece70c50c2927ac68445
Vlad Shapovalov <newshapovalov2002@gmail.com>
2020-04-30 15:05:52 +0300

    Small part, only start making

---
+++ components/shop/goodDetailsWizardAdd.js
@@ -0,0 +1,18 @@
+const GoodDetailsWizardAdd = ({}) => {
+  return (
+    <>
+      <div className="addition-wrapper">
+        <span className="addition-title">Додати в комплект:</span>
+        <div className="content-wrapper">
+          <img src={} />
+          <div className="content-description-amount">
+            <span className="content-title"></span>
+          </div>
+        </div>
+      </div>
+      <style jsx>{``}</style>
+    </>
+  );
+};
+
+export default GoodDetailsWizardAdd;
