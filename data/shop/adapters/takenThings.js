e9f99da543f3160b56930df5c11b77817b78d78a
Vlad Shapovalov <newshapovalov2002@gmail.com>
2020-05-01 19:27:19 +0300

    Add things which customer also can add

---
+++ data/shop/adapters/takenThings.js
@@ -0,0 +1,30 @@
+const takenThings = {
+  largeImg: {
+    src: "/img/good-to-complect-img/bike-transparent-2.jpg",
+    srcSet:
+      "/img/good-to-complect-img/bike-transparent-2@2x.jpg 2x, img/good-to-complect-img/bike-transparent-2@3x.jpg 3x",
+  },
+  smallThings: [
+    {
+      src: "/img/good-to-complect-img/box-1-posud.jpg",
+      srcSet:
+        "/img/good-to-complect-img/box-1-posud.jpg@2x.jpg 2x, img/good-to-complect-img/box-1-posud.jpg@3x.jpg 3x",
+    },
+    {
+      src: "/img/good-to-complect-img/box-2-namet.jpg",
+      srcSet:
+        "/img/good-to-complect-img/box-2-namet.jpg@2x.jpg 2x, img/good-to-complect-img/box-2-namet.jpg@3x.jpg 3x",
+    },
+    {
+      src: "/img/good-to-complect-img/box-3-spalnyk.jpg",
+      srcSet:
+        "/img/good-to-complect-img/box-3-spalnyk.jpg@2x.jpg 2x, img/good-to-complect-img/box-3-spalnyk.jpg@3x.jpg 3x",
+    },
+    {
+      src: "/img/good-to-complect-img/box-4-carimat.jpg",
+      srcSet:
+        "/img/good-to-complect-img/box-4-carimat.jpg@2x.jpg 2x, img/good-to-complect-img/box-4-carimat.jpg@3x.jpg 3x",
+    },
+  ],
+};
+export default takenThings;
