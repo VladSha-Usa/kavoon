0ca307614f18916db8fa0f72733b34743499b5c4
Peter Ovchinnikov <peter@boarlabs.xyz>
2020-05-03 20:57:06 +0300

    Applied eslint rules to data folder

--- data/shop/data/goods1.js
+++ data/shop/data/goods1.js
@@ -1,12 +1,11 @@
 const Goods1 = [
   [
     {
-      name: "Сумка-табус маленька\nна вилку",
+      name: 'Сумка-табус маленька\nна вилку',
       price: 1300,
-      volume: "6л",
+      volume: '6л',
+    },
+  ],
+];
 
-    }
-  ]
-]
-
-export default Goods1
+export default Goods1;
--- data/shop/logic/basket.js
+++ data/shop/logic/basket.js
@@ -1,6 +1,20 @@
+import { BehaviorSubject } from 'rxjs';
+import {
+  map,
+} from 'rxjs/operators';
 
+const goods = new BehaviorSubject([]);
 
-const Basket = {
+const count = goods.pipe(map((goods) => goods.reduce((sum, item) => sum + item.count, 0)));
 
-  count: 1,
+function addGood(good) {
+  console.log(good);
 }
+
+const BasketLogic = {
+  addGood,
+  goods,
+  count,
+};
+
+export default BasketLogic;
