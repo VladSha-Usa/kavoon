de640f0eb45a5a5a428cf61ae39d0377b2fe3b52
Vlad Shapovalov <newshapovalov2002@gmail.com>
2020-05-05 22:23:50 +0300

    Fix quotes according to Eslint

--- data/shop/adapters/good1.js
+++ data/shop/adapters/good1.js
@@ -1,145 +1,145 @@
 const Good1 = {
-  name: "Сумка-табус маленька\nна вилку",
+  name: 'Сумка-табус маленька\nна вилку',
   price: 1300,
   specification: {
-    volume: "6л",
-    height: "40см",
-    materials: "тканина оксфорд, водовідштовхуюча\n блискавка, фурнітура WJ.",
+    volume: '6л',
+    height: '40см',
+    materials: 'тканина оксфорд, водовідштовхуюча\n блискавка, фурнітура WJ.',
     description:
-      "В середині є додаткова кишенька.\nВ клапані - відділення для дрібничок.\nВ комплекті є лямка через плечо.\nПотрібно додаткове кріплення. ",
+      'В середині є додаткова кишенька.\nВ клапані - відділення для дрібничок.\nВ комплекті є лямка через плечо.\nПотрібно додаткове кріплення. ',
     inAddition: [
-      "- можливі різні варіації з кольором\n",
-      "- різні комбінації сумок\n",
-      "- наявність рейнкавера\n",
+      '- можливі різні варіації з кольором\n',
+      '- різні комбінації сумок\n',
+      '- наявність рейнкавера\n',
     ],
   },
   gallery: [
     {
-      src: "/img/good-details-prev-slider/photo-main1.jpg",
+      src: '/img/good-details-prev-slider/photo-main1.jpg',
       srcSet:
-        "/img/good-details-prev-slider/photo-main1@2x.jpg img/good-details-prev-slider/photo-main1@3x.jpg",
+        '/img/good-details-prev-slider/photo-main1@2x.jpg img/good-details-prev-slider/photo-main1@3x.jpg',
     },
     {
-      src: "/img/good-details-prev-slider/photo-main2.jpg",
+      src: '/img/good-details-prev-slider/photo-main2.jpg',
       srcSet:
-        "/img/good-details-prev-slider/photo-main2@2x.jpg img/good-details-prev-slider/photo-main2@3x.jpg",
+        '/img/good-details-prev-slider/photo-main2@2x.jpg img/good-details-prev-slider/photo-main2@3x.jpg',
     },
     {
-      src: "/img/good-details-prev-slider/photo-main3.jpg",
+      src: '/img/good-details-prev-slider/photo-main3.jpg',
       srcSet:
-        "/img/good-details-prev-slider/photo-main3@2x.jpg img/good-details-prev-slider/photo-main3@3x.jpg",
+        '/img/good-details-prev-slider/photo-main3@2x.jpg img/good-details-prev-slider/photo-main3@3x.jpg',
     },
     {
-      src: "/img/good-details-prev-slider/photo-main4.jpg",
+      src: '/img/good-details-prev-slider/photo-main4.jpg',
       srcSet:
-        "/img/good-details-prev-slider/photo-main4@2x.jpg img/good-details-prev-slider/photo-main4@3x.jpg",
+        '/img/good-details-prev-slider/photo-main4@2x.jpg img/good-details-prev-slider/photo-main4@3x.jpg',
     },
     {
-      src: "/img/good-details-prev-slider/photo-main5.jpg",
+      src: '/img/good-details-prev-slider/photo-main5.jpg',
       srcSet:
-        "/img/good-details-prev-slider/photo-main5@2x.jpg img/good-details-prev-slider/photo-main5@3x.jpg",
+        '/img/good-details-prev-slider/photo-main5@2x.jpg img/good-details-prev-slider/photo-main5@3x.jpg',
     },
     {
-      src: "/img/good-details-prev-slider/photo-main6.jpg",
+      src: '/img/good-details-prev-slider/photo-main6.jpg',
       srcSet:
-        "/img/good-details-prev-slider/photo-main6@2x.jpg img/good-details-prev-slider/photo-main6@3x.jpg",
+        '/img/good-details-prev-slider/photo-main6@2x.jpg img/good-details-prev-slider/photo-main6@3x.jpg',
     },
   ],
   additionGoodToCompl: {
     picture: {
-      src: "/img/good-to-complect-img/flash-light-petzl-tikkid.jpg",
+      src: '/img/good-to-complect-img/flash-light-petzl-tikkid.jpg',
       srcSet:
-        "/img/good-to-complect-img/flash-light-petzl-tikkid@2x.jpg 2x, img/good-to-complect-img/flash-light-petzl-tikkid@3x.jpg 3x",
+        '/img/good-to-complect-img/flash-light-petzl-tikkid@2x.jpg 2x, img/good-to-complect-img/flash-light-petzl-tikkid@3x.jpg 3x',
     },
-    name: "Ліхтарик Petzl Tikkid",
-    price: "800 грн",
+    name: 'Ліхтарик Petzl Tikkid',
+    price: '800 грн',
   },
   detailsImgGallery: [
     {
-      src: "/img/good-details-img-slider/photo-slider-2.jpg",
+      src: '/img/good-details-img-slider/photo-slider-2.jpg',
       srcSet:
-        "/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x",
-      subText: "Всі речі які ми брали з собою",
+        '/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x',
+      subText: 'Всі речі які ми брали з собою',
     },
     {
-      src: "/img/good-details-img-slider/photo-slider-2.jpg",
+      src: '/img/good-details-img-slider/photo-slider-2.jpg',
       srcSet:
-        "/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x",
-      subText: "Дорога до водоспаду",
+        '/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x',
+      subText: 'Дорога до водоспаду',
     },
     {
-      src: "/img/good-details-img-slider/photo-slider-2.jpg",
+      src: '/img/good-details-img-slider/photo-slider-2.jpg',
       srcSet:
-        "/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x",
-      subText: "По дорозі, із запакованими речами",
+        '/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x',
+      subText: 'По дорозі, із запакованими речами',
     },
     {
-      src: "/img/good-details-img-slider/photo-slider-2.jpg",
+      src: '/img/good-details-img-slider/photo-slider-2.jpg',
       srcSet:
-        "/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x",
-      subText: "Зручна сумка на пояс",
+        '/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x',
+      subText: 'Зручна сумка на пояс',
     },
     {
-      src: "/img/good-details-img-slider/photo-slider-2.jpg",
+      src: '/img/good-details-img-slider/photo-slider-2.jpg',
       srcSet:
-        "/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x",
-      subText: "Дорога до водоспаду",
+        '/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x',
+      subText: 'Дорога до водоспаду',
     },
     {
-      src: "/img/good-details-img-slider/photo-slider-2.jpg",
+      src: '/img/good-details-img-slider/photo-slider-2.jpg',
       srcSet:
-        "/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x",
-      subText: "Всі речі які ми брали з собою",
+        '/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x',
+      subText: 'Всі речі які ми брали з собою',
     },
     {
-      src: "/img/good-details-img-slider/photo-slider-2.jpg",
+      src: '/img/good-details-img-slider/photo-slider-2.jpg',
       srcSet:
-        "/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x",
-      subText: "Дорога до водоспаду",
+        '/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x',
+      subText: 'Дорога до водоспаду',
     },
     {
-      src: "/img/good-details-img-slider/photo-slider-2.jpg",
+      src: '/img/good-details-img-slider/photo-slider-2.jpg',
       srcSet:
-        "/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x",
-      subText: "По дорозі, із запакованими речами",
+        '/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x',
+      subText: 'По дорозі, із запакованими речами',
     },
     {
-      src: "/img/good-details-img-slider/photo-slider-2.jpg",
+      src: '/img/good-details-img-slider/photo-slider-2.jpg',
       srcSet:
-        "/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x",
-      subText: "Зручна сумка на пояс",
+        '/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x',
+      subText: 'Зручна сумка на пояс',
     },
     {
-      src: "/img/good-details-img-slider/photo-slider-2.jpg",
+      src: '/img/good-details-img-slider/photo-slider-2.jpg',
       srcSet:
-        "/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x",
-      subText: "Дорога до водоспаду",
+        '/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, img/good-details-img-slider/photo-slider-2@3x.jpg 3x',
+      subText: 'Дорога до водоспаду',
     },
   ],
   moreGoods: [
     {
-      name: "Сумка-бананка\nна кермо",
+      name: 'Сумка-бананка\nна кермо',
       volume: 3,
       price: 1300,
-      image: "/img/img-shop-item/photo-5.jpg",
+      image: '/img/img-shop-item/photo-5.jpg',
     },
     {
-      name: "Бананка",
+      name: 'Бананка',
       volume: 10,
       price: 1300,
-      image: "/img/img-shop-item/photo-6.jpg",
+      image: '/img/img-shop-item/photo-6.jpg',
     },
     {
-      name: "Внутрірамна сумка",
+      name: 'Внутрірамна сумка',
       volume: 10,
       price: 1300,
-      image: "/img/img-shop-item/photo-7.jpg",
+      image: '/img/img-shop-item/photo-7.jpg',
     },
     {
-      name: "Сумка-тубус\nна вилку",
+      name: 'Сумка-тубус\nна вилку',
       volume: 10,
       price: 1300,
-      image: "/img/img-shop-item/photo-8.jpg",
+      image: '/img/img-shop-item/photo-8.jpg',
     },
   ],
 };
