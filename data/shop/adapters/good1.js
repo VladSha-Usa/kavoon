644b0daa6cc0829a66cec3a87e2cae0234d25e1a
Vlad Shapovalov <newshapovalov2002@gmail.com>
2020-05-31 12:48:37 +0300

    Remake popup according to new design

--- data/shop/adapters/good1.js
+++ data/shop/adapters/good1.js
@@ -96,6 +96,33 @@ const Good1 = {
       subText: 'Дорога до водоспаду',
     },
   ],
+  discountIfComplect: 10,
+  allKitDiscount: [
+    {
+      name: 'Сумка-тубус на вилку.',
+      volume: 10,
+      price: 800,
+      src: '/img/good-to-complect-img/photo-1.jpg',
+      srcSet:
+        '/img/good-to-complect-img/photo-1@2x.jpg 2x, img/good-to-complect-img/photo-1@3x.jpg 3x',
+    },
+    {
+      name: 'Сумка-тубус на вилку.',
+      volume: 10,
+      price: 600,
+      src: '/img/good-to-complect-img/photo-1.jpg',
+      srcSet:
+        '/img/good-to-complect-img/photo-1@2x.jpg 2x, img/good-to-complect-img/photo-1@3x.jpg 3x',
+    },
+    {
+      name: 'Сумка-тубус на вилку.',
+      volume: 10,
+      price: 400,
+      src: '/img/good-to-complect-img/photo-1.jpg',
+      srcSet:
+        '/img/good-to-complect-img/photo-1@2x.jpg 2x, img/good-to-complect-img/photo-1@3x.jpg 3x',
+    },
+  ],
   moreGoods: [
     {
       name: 'Сумка-бананка\nна кермо',
