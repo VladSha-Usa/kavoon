f77a628ec5b96801f662f578ce9acf6ca9ce7e40
Vlad Shapovalov <newshapovalov2002@gmail.com>
2020-07-14 17:15:38 +0300

    Rename good1 to triangularBicycleFirstAidKitFrame

--- pages/shop/tringular-bicycle-first-aid-kit-frame.js
+++ pages/shop/tringular-bicycle-first-aid-kit-frame.js
@@ -2,6 +2,8 @@ import GoodPage from '../../components/goodPage';
 import vmFactory from '../../data/viewModels/goodVM';
 import { bagBigFork } from '../../data/data/goods';
 
-const Good1 = () => <GoodPage vm={vmFactory(bagBigFork)} />;
+const TriangularBicycleFirstAidKitFrame = () => (
+  <GoodPage vm={vmFactory(bagBigFork)} />
+);
 
-export default Good1;
+export default TriangularBicycleFirstAidKitFrame;
