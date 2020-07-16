399f85b10486df8e97ba6efc77a045d4113a06d6
Vlad Shapovalov <newshapovalov2002@gmail.com>
2020-07-16 13:58:10 +0300

    Correct code of goodDetailsWizard

--- data/viewModels/good/GoodDetailsWizardVM.js
+++ data/viewModels/good/GoodDetailsWizardVM.js
@@ -1,18 +1,16 @@
 import basketModel from '../../logic/basket';
 import goodDetailsWizardTitleVM from './GoodDetailsWizardTitleVM';
-import goodDetailsWizardPickerVM from './GoodDetailsWizardPickerVM';
 import goodDetailsWizardAddOtherVM from './GoodDetailsWizardAddOtherVM';
 import goodDetailsOrderBtnVM from './GoodDetailsOrderBtnVM';
 
 export default (good) => ({
   colectDataOfGood: basketModel.colectDataOfGood,
-  good,
+  fabrics: good.fabrics,
   goodDetailsWizardTitleVM: goodDetailsWizardTitleVM({
     name: good.name,
     price: good.price,
     volume: good.volume,
   }),
-  goodDetailsWizardPickerVM: goodDetailsWizardPickerVM(),
   goodDetailsWizardAddOtherVM: goodDetailsWizardAddOtherVM(),
   goodDetailsOrderBtnVM: goodDetailsOrderBtnVM(),
 });
