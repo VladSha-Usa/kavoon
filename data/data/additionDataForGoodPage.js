14bbba65caa8cba793be90c0c12f8975dc63ac8f
Vlad Shapovalov <newshapovalov2002@gmail.com>
2020-07-17 13:05:09 +0300

    Create file for addition data for page of good

---
+++ data/data/additionDataForGoodPage.js
@@ -0,0 +1,148 @@
+export const additionDataForGoodPage = {
+  takenThings: {
+    largeImg: {
+      src: '/img/good-to-complect-img/bike-transparent-2.jpg',
+      srcSet:
+        '/img/good-to-complect-img/bike-transparent-2@2x.jpg 2x, /img/good-to-complect-img/bike-transparent-2@3x.jpg 3x',
+    },
+    smallThings: [
+      {
+        src: '/img/good-to-complect-img/posud-photo.jpg',
+        srcSet:
+          '/img/good-to-complect-img/posud-photo@2x.jpg 2x, /img/good-to-complect-img/posud-photo@3x.jpg 3x',
+        name: 'Посуд',
+      },
+      {
+        src: '/img/good-to-complect-img/namet-photo.jpg',
+        srcSet:
+          '/img/good-to-complect-img/namet-photo@2x.jpg 2x, /img/good-to-complect-img/namet-photo@3x.jpg 3x',
+        name: 'Намет',
+      },
+      {
+        src: '/img/good-to-complect-img/spalnyk-photo.jpg',
+        srcSet:
+          '/img/good-to-complect-img/spalnyk-photo@2x.jpg 2x, /img/good-to-complect-img/spalnyk-photo@3x.jpg 3x',
+        name: 'Спальник\nx2',
+      },
+      {
+        src: '/img/good-to-complect-img/carimat-photo.jpg',
+        srcSet:
+          '/img/good-to-complect-img/carimat-photo@2x.jpg 2x, /img/good-to-complect-img/carimat-photo@3x.jpg 3x',
+        name: 'Карімат\nнадувний\nx2',
+      },
+      {
+        src: '/img/good-to-complect-img/posud-photo.jpg',
+        srcSet:
+          '/img/good-to-complect-img/posud-photo@2x.jpg 2x, /img/good-to-complect-img/posud-photo@3x.jpg 3x',
+        name: 'Посуд',
+      },
+      {
+        src: '/img/good-to-complect-img/namet-photo.jpg',
+        srcSet:
+          '/img/good-to-complect-img/namet-photo@2x.jpg 2x, /img/good-to-complect-img/namet-photo@3x.jpg 3x',
+        name: 'Намет',
+      },
+      {
+        src: '/img/good-to-complect-img/spalnyk-photo.jpg',
+        srcSet:
+          '/img/good-to-complect-img/spalnyk-photo@2x.jpg 2x, /img/good-to-complect-img/spalnyk-photo@3x.jpg 3x',
+        name: 'Спальник',
+      },
+      {
+        src: '/img/good-to-complect-img/carimat-photo.jpg',
+        srcSet:
+          '/img/good-to-complect-img/carimat-photo@2x.jpg 2x, /img/good-to-complect-img/carimat-photo@3x.jpg 3x',
+        name: 'Карімат\nнадувний',
+      },
+    ],
+  },
+  allKitDiscount: [
+    {
+      name: 'Сумка-тубус на вилку.',
+      volume: 10,
+      price: 800,
+      src: '/img/good-to-complect-img/photo-1.jpg',
+      srcSet:
+        '/img/good-to-complect-img/photo-1@2x.jpg 2x, /img/good-to-complect-img/photo-1@3x.jpg 3x',
+    },
+    {
+      name: 'Сумка-тубус на вилку.',
+      volume: 10,
+      price: 600,
+      src: '/img/good-to-complect-img/photo-1.jpg',
+      srcSet:
+        '/img/good-to-complect-img/photo-1@2x.jpg 2x, /img/good-to-complect-img/photo-1@3x.jpg 3x',
+    },
+    {
+      name: 'Сумка-тубус на вилку.',
+      volume: 10,
+      price: 400,
+      src: '/img/good-to-complect-img/photo-1.jpg',
+      srcSet:
+        '/img/good-to-complect-img/photo-1@2x.jpg 2x, /img/good-to-complect-img/photo-1@3x.jpg 3x',
+    },
+  ],
+  detailsImgGallery: [
+    {
+      src: 'https://via.placeholder.com/300x220.jpg?text=No+Image',
+      srcSet:
+        'https://via.placeholder.com/600x440@2x.jpg?text=No+Image 2x, https://via.placeholder.com/900x660@3x.jpg?text=No+Image  3x',
+      subText: 'Всі речі які ми брали з собою',
+    },
+    {
+      src: '/img/good-details-img-slider/photo-slider-2.jpg',
+      srcSet:
+        '/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, /img/good-details-img-slider/photo-slider-2@3x.jpg 3x',
+      subText: 'Дорога до водоспаду',
+    },
+    {
+      src: 'https://via.placeholder.com/300x220.jpg?text=No+Image',
+      srcSet:
+        'https://via.placeholder.com/600x440@2x.jpg?text=No+Image 2x, https://via.placeholder.com/900x660@3x.jpg?text=No+Image  3x',
+      subText: 'По дорозі, із запакованими речами',
+    },
+    {
+      src: 'https://via.placeholder.com/300x220.jpg?text=No+Image',
+      srcSet:
+        'https://via.placeholder.com/600x440@2x.jpg?text=No+Image 2x, https://via.placeholder.com/900x660@3x.jpg?text=No+Image  3x',
+      subText: 'Зручна сумка на пояс',
+    },
+    {
+      src: '/img/good-details-img-slider/photo-slider-2.jpg',
+      srcSet:
+        '/img/good-details-img-slider/photo-slider-2@2x.jpg 2x, /img/good-details-img-slider/photo-slider-2@3x.jpg 3x',
+      subText: 'Дорога до водоспаду',
+    },
+  ],
+  moreGoods: [
+    {
+      name: 'Сумка-бананка\nна кермо',
+      volume: 3,
+      price: 1300,
+      image: '/shop/photo-5.jpg',
+      srcSet: '/shop/photo-5@2x.jpg 2x, /shop/photo-5@3x.jpg 3x',
+    },
+    {
+      name: 'Бананка',
+      volume: 10,
+      price: 1300,
+      image: '/shop/photo-6.jpg',
+      srcSet: '/shop/photo-6@2x.jpg 2x, /shop/photo-6@3x.jpg 3x',
+    },
+    {
+      name: 'Внутрірамна сумка',
+      volume: 10,
+      price: 1300,
+      image: '/shop/Sumka_trykutna_vnytriramna.jpg',
+      srcSet:
+        '/shop/Sumka_trykutna_vnytriramna@2x.jpg 2x, /shop/Sumka_trykutna_vnytriramna@3x.jpg 3x',
+    },
+    {
+      name: 'Сумка-тубус\nна вилку',
+      volume: 10,
+      price: 1300,
+      image: '/shop/photo-8.jpg',
+      srcSet: '/shop/photo-8@2x.jpg 2x, /shop/photo-8@3x.jpg 3x',
+    },
+  ],
+};
