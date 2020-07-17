7dc4700440cba1f3c4abc1588e2be6cca7ec93ff
Vlad Shapovalov <newshapovalov2002@gmail.com>
2020-07-17 17:26:44 +0300

    Remake structure of files, add function moreGoods

---
+++ data/data/allKitDiscount.js
@@ -0,0 +1,28 @@
+const allKitDiscount = [
+  {
+    name: 'Сумка-тубус на вилку.',
+    volume: 10,
+    price: 800,
+    src: '/img/good-to-complect-img/photo-1.jpg',
+    srcSet:
+      '/img/good-to-complect-img/photo-1@2x.jpg 2x, /img/good-to-complect-img/photo-1@3x.jpg 3x',
+  },
+  {
+    name: 'Сумка-тубус на вилку.',
+    volume: 10,
+    price: 600,
+    src: '/img/good-to-complect-img/photo-1.jpg',
+    srcSet:
+      '/img/good-to-complect-img/photo-1@2x.jpg 2x, /img/good-to-complect-img/photo-1@3x.jpg 3x',
+  },
+  {
+    name: 'Сумка-тубус на вилку.',
+    volume: 10,
+    price: 400,
+    src: '/img/good-to-complect-img/photo-1.jpg',
+    srcSet:
+      '/img/good-to-complect-img/photo-1@2x.jpg 2x, /img/good-to-complect-img/photo-1@3x.jpg 3x',
+  },
+];
+
+export default allKitDiscount;
