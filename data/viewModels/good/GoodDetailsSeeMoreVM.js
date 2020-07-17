0be5bf69339016ffe49bed6e501e537f0f61159e
Vlad Shapovalov <newshapovalov2002@gmail.com>
2020-07-17 18:20:16 +0300

    CHange randomizer, fix warning by adding isClient

--- data/viewModels/good/GoodDetailsSeeMoreVM.js
+++ data/viewModels/good/GoodDetailsSeeMoreVM.js
@@ -1,33 +1,21 @@
-export default () => ({
-  moreGoods: [
-    {
-      name: 'Сумка-бананка\nна кермо',
-      volume: 3,
-      price: 1300,
-      image: '/shop/photo-5.jpg',
-      srcSet: '/shop/photo-5@2x.jpg 2x, /shop/photo-5@3x.jpg 3x',
-    },
-    {
-      name: 'Бананка',
-      volume: 10,
-      price: 1300,
-      image: '/shop/photo-6.jpg',
-      srcSet: '/shop/photo-6@2x.jpg 2x, /shop/photo-6@3x.jpg 3x',
-    },
-    {
-      name: 'Внутрірамна сумка',
-      volume: 10,
-      price: 1300,
-      image: '/shop/Sumka_trykutna_vnytriramna.jpg',
-      srcSet:
-        '/shop/Sumka_trykutna_vnytriramna@2x.jpg 2x, /shop/Sumka_trykutna_vnytriramna@3x.jpg 3x',
-    },
-    {
-      name: 'Сумка-тубус\nна вилку',
-      volume: 10,
-      price: 1300,
-      image: '/shop/photo-8.jpg',
-      srcSet: '/shop/photo-8@2x.jpg 2x, /shop/photo-8@3x.jpg 3x',
-    },
-  ],
-});
+export default (Goods) => {
+  let n = 4;
+  var result = new Array(n),
+    len = Goods.length,
+    taken = new Array(len);
+  while (n--) {
+    var x = Math.floor(Math.random() * len);
+    result[n] = Goods[x in taken ? taken[x] : x];
+    taken[x] = --len in taken ? taken[len] : len;
+  }
+  return result.map((goodData) => {
+    return {
+      name: goodData.name,
+      volume: goodData.volume,
+      price: goodData.price,
+      image: goodData.image,
+      srcSet: goodData.srcSet,
+      href: `/shop/${goodData.id}`,
+    };
+  });
+};
